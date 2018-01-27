881d0fbac101edee1df0dd1e531622f6f1839cee
Keith Rieck <KeithRieck@gmail.com>
2018-01-27 11:08:44 -0600

    Added autonomous commands.

--- src/main/java/org/firebears/betaTestRobot4/commands/RightSwitchPriorityCommand.java
+++ src/main/java/org/firebears/betaTestRobot4/commands/RightSwitchPriorityCommand.java
@@ -4,8 +4,14 @@ import org.firebears.betaTestRobot4.RobotConfig;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.PrintCommand;
 
+/**
+ * Autonomous command for scoring on the switch from the right side of the field.
+ */
 public class RightSwitchPriorityCommand extends AbstractAutoCommand {
 
+  /**
+   * @return the best command for handling the game data.
+   */
   protected Command pickAutoCommand(String data) {
     if (data.startsWith("L")) {
       return new TurnCommand(RobotConfig.chassis, true);
@@ -16,6 +22,9 @@ public class RightSwitchPriorityCommand extends AbstractAutoCommand {
     }
   }
 
+  /**
+   * @return a command to start if we never get game data.
+   */
   protected Command getTimeoutCommand() {
     return new PrintCommand("DriveStraightCommand");
   }
